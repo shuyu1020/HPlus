de2fb957c6355da16bee26ae772bb3bd6d981826
shuyu1020 <m10711009@gapps.ntust.edu.tw>
2020-06-09 13:22:57 +0800

    0531

---
+++ HPlus_0531/Time/TimeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimeViewController.swift
+//  HPlus_0531
+//
+//  Created by KAREN_JIANG on 2020/6/7.
+//  Copyright © 2020 KAREN_JIANG. All rights reserved.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- HPlus_0531/ViewController.swift
+++ HPlus_0531/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let fetures : [String] = ["WebView Load URL", "WebView Load HTML", "WebView Interaction", "SFSafariViewController"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+}
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let name = self.fetures[ indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: FeatureViewCell.identifier ) as! FeatureViewCell
+        
+        cell.updateContent(title: name)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch indexPath.row {
+        case 0:
+            self.performSegue(withIdentifier: "moveToURL", sender: self)
+            break
+        case 1:
+            self.performSegue(withIdentifier: "moveToHTML", sender: self)
+            break
+        case 2:
+            self.performSegue(withIdentifier: "moveToInteraction", sender: self)
+            break
+        case 3:
+            break
+        default:
+            break
+        }
+    }
 }
 
